30da32c1cd058bd6f64ad5693a640187183ae0be
ig1711 <cgrecent@gmail.com>
2022-04-06 12:14:26 +0600

    removes multithread

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,32 @@
 use png;
-use png_blur::tttt;
+use stackblur::stackblur;
 use std::fs::File;
 use std::io::BufWriter;
 use std::path::Path;
 use std::time::SystemTime;
 
 fn main() {
-    // The decoder is a build for reader and can be used to set various decoding options
-    // via `Transformations`. The default output transformation is `Transformations::IDENTITY`.
-    let decoder = png::Decoder::new(File::open("l.png").unwrap());
+    let decoder = png::Decoder::new(File::open("in.png").unwrap());
     let mut reader = decoder.read_info().unwrap();
-    // Allocate the output buffer.
     let mut buf = vec![0; reader.output_buffer_size()];
-    // Read the next frame. An APNG might contain multiple frames.
     let info = reader.next_frame(&mut buf).unwrap();
-    // Grab the bytes of the image.
     let bytes = &buf[..info.buffer_size()];
+
     let stime = SystemTime::now();
-    let hmm = tttt::mt_blur(bytes, info.width as usize, info.height as usize, 4, 23);
-    // println!("{:?}", hmm);
-    println!("{:?}", stime.elapsed().unwrap().as_millis());
-    let _hmm2 = tttt::blur(bytes, info.width as usize, info.height as usize, 4, 23);
-    // println!("{:?}", hmm);
+    let hmm = stackblur::blur(bytes, info.width as usize, info.height as usize, 4, 23);
     println!("{:?}", stime.elapsed().unwrap().as_millis());
 
-    let path = Path::new(r"./image4.png");
+    let path = Path::new(r"./out.png");
     let file = File::create(path).unwrap();
     let ref mut w = BufWriter::new(file);
 
-    let mut encoder = png::Encoder::new(w, info.width, info.height); // Width is 2 pixels and height is 1.
+    let mut encoder = png::Encoder::new(w, info.width, info.height);
     encoder.set_color(png::ColorType::Rgba);
     encoder.set_depth(png::BitDepth::Eight);
     encoder.set_trns(vec![0xFFu8, 0xFFu8, 0xFFu8, 0xFFu8]);
-    encoder.set_source_gamma(png::ScaledFloat::from_scaled(45455)); // 1.0 / 2.2, scaled by 100000
-    encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2)); // 1.0 / 2.2, unscaled, but rounded
+    encoder.set_source_gamma(png::ScaledFloat::from_scaled(45455));
+    encoder.set_source_gamma(png::ScaledFloat::new(1.0 / 2.2));
     let source_chromaticities = png::SourceChromaticities::new(
-        // Using unscaled instantiation here
         (0.31270, 0.32900),
         (0.64000, 0.33000),
         (0.30000, 0.60000),
@@ -44,6 +35,5 @@ fn main() {
     encoder.set_source_chromaticities(source_chromaticities);
     let mut writer = encoder.write_header().unwrap();
 
-    // let data = [255, 0, 0, 255, 0, 0, 0, 255]; // An array containing a RGBA sequence. First pixel is red and second pixel is black.
-    writer.write_image_data(&hmm[..]).unwrap(); // Save
+    writer.write_image_data(&hmm[..]).unwrap();
 }
